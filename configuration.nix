33e27efd988614ec1ada117efe83a4ca618caab0
InfinityGhost <39218853+InfinityGhost@users.noreply.github.com>
2020-09-09 22:51:04 -0400

    Configuration cleanup

--- configuration.nix
+++ configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
@@ -13,17 +9,15 @@
       ./nfs.nix
     ];
 
-  # Use the GRUB 2 boot loader.
+  # GRUB 2
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
   boot.loader.grub.device = "/dev/sda";
 
   # Network
-  networking.hostName = "nixos-server"; # Define your hostname.
+  networking.hostName = "nixos-server";
 
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
+  # DHCP on network interface enp3s0
   networking.useDHCP = false;
   networking.interfaces.enp3s0.useDHCP = true;
 
@@ -37,8 +31,7 @@
   # Set your time zone.
   time.timeZone = "America/New_York";
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+  # System packages
   nixpkgs.config.allowUnfree = true;
   environment.systemPackages = with pkgs; [
     wget
@@ -74,13 +67,8 @@
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "20.03"; # Did you read the comment?
+  # NixOS release
+  system.stateVersion = "20.03";
 
 }
 
