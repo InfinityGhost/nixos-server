4260844bf6094bcaaf8ca0c0bcce469e99f042ad
infinity <39218853+InfinityGhost@users.noreply.github.com>
2020-09-12 22:22:52 -0400

    Allow all local IPs for NFS

--- nfs.nix
+++ nfs.nix
@@ -8,6 +8,6 @@
   # Server configuration
   services.nfs.server.enable = true;
   services.nfs.server.exports = ''
-    /export/media 192.168.0.2(rw,no_subtree_check) 192.168.0.27(rw,no_subtree_check)
+    /export/media 192.168.0.0/24(rw,no_subtree_check)
   '';
 }
